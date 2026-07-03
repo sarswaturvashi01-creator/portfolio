18c496b5c755efd83337ee3b190f79343acdb6c9
urvashi sarswat <sarswaturvashi01@gmail.com>
2026-07-03 05:33:17 -0700

    make mobile responsive

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-    const links = document.querySelectorAll('.nav-links a');
-    links.forEach(link => {
-        link.addEventListener('click', function(){
-            links.forEach(l => l.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-</script>
